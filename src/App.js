a858bdc1825b4413dd218a4a94381d5b745680ec
amooryjubran <omarsalehjubran@gmail.com>
2020-05-25 03:50:33 -0400

    updaing shop page, collection preview, collection item

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {Route, Switch} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/Shop/shop.component';
 
 const HatsPage = () => (
   <div>
@@ -16,7 +17,8 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route  path='/hats' component={HatsPage}/>
+        <Route  path='/shop' component={ShopPage}/>
+        
       </Switch>
     </div>
   );
